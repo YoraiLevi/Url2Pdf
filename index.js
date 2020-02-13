249ecd7e447e2636a0078eb0119e1d299e9ad5c5
Yorai Levi <Yorai.email@gmail.com>
2020-02-13 05:26:06 +0200

    added eslint support

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /**
  * @name pdf
  *
@@ -6,116 +7,160 @@
  * @see {@link https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pdf}
  */
 
-const commandLineArgs = require('command-line-args')
+const commandLineArgs = require('command-line-args');
 const mkdirp = require('mkdirp');
 const yesno = require('yesno');
 const puppeteer = require('puppeteer');
 const path = require('path');
 
 
-DEFAULT_OUT = './out'
-DEFAULT_TIMEOUT = 3 * 60 * 1000
+DEFAULT_OUT = './out';
+DEFAULT_TIMEOUT = 3 * 60 * 1000;
 const optionDefinitions = [
-	{ name: 'verbose', alias: 'v', type: Boolean, multiple: true, defaultValue: [] },
-	{ name: 'silent', alias: 's', type: Boolean, multiple: true, defaultValue: [] },
-	{ name: 'mute', alias: 'm', type: Boolean, multiple: false, defaultValue: false },
-	{ name: 'urls', type: String, alias: 'u', multiple: true, defaultOption: true, defaultValue: [] },
-	{ name: 'out', type: String, alias: 'o', defaultValue: DEFAULT_OUT },
-	{ name: 'timeout', alias: 't', type: Number, defaultValue: DEFAULT_TIMEOUT },
-	{ name: 'autoAccept', alias: 'y', type: Boolean, defaultValue: false }
+  {name: 'verbose', alias: 'v', type: Boolean, multiple: true, defaultValue: []},
+  {name: 'silent', alias: 's', type: Boolean, multiple: true, defaultValue: []},
+  {name: 'mute', alias: 'm', type: Boolean, multiple: false, defaultValue: false},
+  {name: 'urls', type: String, alias: 'u', multiple: true, defaultOption: true, defaultValue: []},
+  {name: 'out', type: String, alias: 'o', defaultValue: DEFAULT_OUT},
+  {name: 'timeout', alias: 't', type: Number, defaultValue: DEFAULT_TIMEOUT},
+  {name: 'autoAccept', alias: 'y', type: Boolean, defaultValue: false},
+];
+const options = commandLineArgs(optionDefinitions);
+options['out'] = options['out'] || DEFAULT_OUT;
+options['out'] = path.resolve(options['out']);
+options['verbose'] = options['silent'].length >= 1 ? [] : options['verbose'];
+options['timeout'] = options['timeout'] || DEFAULT_TIMEOUT;
+const printLevel = options.mute ? -Infinity : options.verbose.length - options.silent.length;
+const log = (...args) => {
+  if (printLevel >= 0) {
+    console.log(...args);
+  }
+};
+const info = (...args) => {
+  if (printLevel >= 1) {
+    console.info(...args);
+  }
+};
+const debug = (...args) => {
+  if (printLevel >= 2) {
+    console.debug(...args);
+  }
+};
+const error = (...args) => {
+  if (printLevel >= -3) {
+    console.error(...args);
+  }
+};
+const warn = (...args) => {
+  if (printLevel >= -2) {
+    console.warn(...args);
+  }
+};
 
-]
-var options = commandLineArgs(optionDefinitions)
-options['out'] = options['out'] || DEFAULT_OUT
-options['out'] = path.resolve(options['out'])
-options['verbose'] = options['silent'].length >= 1 ? [] : options['verbose']
-options['timeout'] = options['timeout'] || DEFAULT_TIMEOUT
-var printLevel = options.mute ? -Infinity : options.verbose.length - options.silent.length
-log = (...args) => { if (printLevel >= 0) { console.log(...args) } }
-info = (...args) => { if (printLevel >= 1) { console.info(...args) } }
-debug = (...args) => { if (printLevel >= 2) { console.debug(...args) } }
-error = (...args) => { if (printLevel >= -3) { console.error(...args) } }
-warn = (...args) => { if (printLevel >= -2) { console.warn(...args) } }
+// Global browser
+let browser;
+
+/**
+ * Verify with the user if everything is well.
+ * @return {boolean} is everything setup correctly?
+ */
 async function verify() {
-	log(options['urls'].length, "Urls")
-	log("Timeout is set to:", options['timeout'])
-	log("Output is set to:", options['out'])
+  log(options['urls'].length, 'Urls');
+  log('Timeout is set to:', options['timeout']);
+  log('Output is set to:', options['out']);
 
-	if (!options['autoAccept']) {
-		const ok = await yesno({
-			question: 'Do you wish to crawl?'
-		});
-		return ok
-	}
-	else return true
+  if (!options['autoAccept']) {
+    const ok = await yesno({
+      question: 'Do you wish to crawl?',
+    });
+    return ok;
+  } else return true;
 }
-
+/**
+ * Initializes components.
+ * @return {void}
+ */
 async function init() {
-	mkdirp.sync(options['out'])
-	browser = await puppeteer.launch({ 'headless': true })
+  mkdirp.sync(options['out']);
+  browser = await puppeteer.launch({'headless': true});
 }
+/**
+ * Tears down components.
+ * @return {void}
+ */
 async function teardown() {
-	log("Exiting.")
-	try {
-		await browser.close()
-	}
-	catch{
-		process.exit()
-	}
+  log('Exiting.');
+  try {
+    await browser.close();
+  } finally {
+    process.exit();
+  }
 }
+/**
+ * Replace disallowed chareacters with UNDERSCORE.
+ * @param {string} url
+ * @return {string} valid file name
+ */
 function generateNameFromUrl(url) {
-	// Replace disallowed chareacters with UNDERSCORE
-	uniqueName = url.replace("://", "_")
-		.replace(/\./g, "_")
-		.replace(/\//g, "_")
-		.replace(/\?/g, "_")
-		.replace(/\*/g, "_")
-		.replace(/\"/g, "_")
-		.replace(/\</g, "_")
-		.replace(/\>/g, "_")
-		.replace(/\|/g, "_")
-	return uniqueName;
+  uniqueName = url.replace('://', '_')
+      .replace(/\./g, '_')
+      .replace(/\//g, '_')
+      .replace(/\?/g, '_')
+      .replace(/\*/g, '_')
+      .replace(/\"/g, '_')
+      .replace(/\</g, '_')
+      .replace(/\>/g, '_')
+      .replace(/\|/g, '_');
+  return uniqueName;
 }
+/**
+ * Main.
+ * @return {void}
+ */
 async function main() {
-	try {
-		const page = await browser.newPage()
-		var resultStatus = {}
-		for (url of options['urls']) {
-			resultStatus[url] = await UrlAsPdf(page, url, options['out'])
-		}
-	}
-	finally {
-		entries = Object.entries(resultStatus);
-		failed = entries.map(x => { if (!x[1]) return x[0]; }).filter(x => x)
-		log("Successes:" + (entries.length - failed.length) + "/" + entries.length)
-		if (failed.length > 0) {
-			warn("Failed:\n", ...failed)
-		}
-		else log("Yay! everything completed without any failure")
-	}
+  const resultStatus = {};
+  try {
+    const page = await browser.newPage();
+    for (url of options['urls']) {
+      resultStatus[url] = await urlAsPdf(page, url, options['out']);
+    }
+  } finally {
+    entries = Object.entries(resultStatus);
+    failed = entries.map((x) => {
+      if (!x[1]) return x[0];
+    }).filter((x) => x);
+    log('Successes:' + (entries.length - failed.length) + '/' + entries.length);
+    if (failed.length > 0) {
+      warn('Failed:\n', ...failed);
+    } else log('Yay! everything completed without any failure');
+  }
 }
-async function UrlAsPdf(page, url, outdir) {
-	let name = generateNameFromUrl(url)
-	save_path = path.resolve(outdir, name)
-	try {
-		info("Crawling:", url)
-		await page.goto(url, { waitUntil: "networkidle0", timeout: options['timeout'] })
-		await page.emulateMedia('screen');
-		await page.pdf({ path: save_path + '.pdf', format: 'A4', printBackground: true, })
-		return true
-	}
-	catch (err) {
-		error("Url Failed:", url, "\n", "Reason:", err.message)
-		return false
-	}
+/**
+ * Crawls a url and saves it as pdf in specified directory
+ * @param {*} page puppeteer's Page
+ * @param {*} url to crawl
+ * @param {*} outdir
+ */
+async function urlAsPdf(page, url, outdir) {
+  const name = generateNameFromUrl(url);
+  const savePath = path.resolve(outdir, name);
+  try {
+    info('Crawling:', url);
+    await page.goto(url, {waitUntil: 'networkidle0', timeout: options['timeout']});
+    await page.emulateMedia('screen');
+    await page.pdf({path: savePath + '.pdf', format: 'A4', printBackground: true});
+    return true;
+  } catch (err) {
+    error('Url Failed:', url, '\n', 'Reason:', err.message);
+    return false;
+  }
 }
-//runner
+// runner
 (async () => {
-	var browser;
-	debug("options:", options)
-	if (await verify()) {
-		await init()
-		await main()
-	}
-	await teardown()
-})()
+  debug('options:', options);
+  if (await verify()) {
+    await init();
+    await main();
+  }
+  await teardown();
+})();
